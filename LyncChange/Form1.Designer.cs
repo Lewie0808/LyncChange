de9c368b86615a3ca5f688d9085ccf4fcc535003
jlew0808@yahoo.com <jlew0808@yahoo.com>
2017-08-09 09:40:14 +0100

    Added button colour

--- LyncChange/Form1.Designer.cs
+++ LyncChange/Form1.Designer.cs
@@ -28,12 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnChange = new System.Windows.Forms.Button();
+            this.btnDefault = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnChange
+            // 
+            this.btnChange.Location = new System.Drawing.Point(42, 22);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(195, 23);
+            this.btnChange.TabIndex = 2;
+            this.btnChange.Text = "Change to 360 mins";
+            this.btnChange.UseVisualStyleBackColor = true;
+            this.btnChange.Click += new System.EventHandler(this.BtnChange_Click);
+            // 
+            // btnDefault
+            // 
+            this.btnDefault.Location = new System.Drawing.Point(42, 65);
+            this.btnDefault.Name = "btnDefault";
+            this.btnDefault.Size = new System.Drawing.Size(195, 23);
+            this.btnDefault.TabIndex = 3;
+            this.btnDefault.Text = "Default to 5 mins";
+            this.btnDefault.UseVisualStyleBackColor = true;
+            this.btnDefault.Click += new System.EventHandler(this.BtnDefault_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(97, 109);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(275, 148);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnDefault);
+            this.Controls.Add(this.btnChange);
+            this.Name = "Form1";
+            this.Text = "Lync Change";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Button btnChange;
+        private System.Windows.Forms.Button btnDefault;
+        private System.Windows.Forms.Button btnClose;
     }
 }
 
--- LyncChange/Form1.cs
+++ LyncChange/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,37 @@ namespace LyncChange
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void BtnChange_Click(object sender, EventArgs e)
+        {
+            RegistryKey Lync = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Office\16.0\Lync", true);
+            if(Lync != null)
+            {
+                Lync.SetValue("AwayThreshold", "360", RegistryValueKind.DWord);
+                Lync.SetValue("IdleThreshold", "360", RegistryValueKind.DWord);
+                Lync.Close();
+            }
+            btnChange.BackColor = Color.Green;
+            btnDefault.BackColor = default(Color);
+        }
+
+        private void BtnDefault_Click(object sender, EventArgs e)
+        {
+            RegistryKey Lync = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Office\16.0\Lync", true);
+            if (Lync != null)
+            {
+                Lync.SetValue("AwayThreshold", "5", RegistryValueKind.DWord);
+                Lync.SetValue("IdleThreshold", "5", RegistryValueKind.DWord);
+                Lync.Close();
+            }
+            btnChange.BackColor =  default(Color);
+            btnDefault.BackColor = Color.Green;
+        }
+
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
